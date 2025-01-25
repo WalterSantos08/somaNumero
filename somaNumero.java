976fa136297f473a68a885808e8dbdb09aab72f2
Walter Santos <waltersantosfilho94@gmail.com>
2025-01-25 18:36:22 -0300

    Add files via upload

---
+++ somaNumero.java
@@ -0,0 +1,25 @@
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.*/
+
+import java.util.Scanner;
+
+public class somaNumero {
+
+	public static void main(String[] args) {
+		Scanner dados = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro numero: ");
+		int numero1 = dados.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		int numero2 = dados.nextInt();
+		
+		int soma = numero1 + numero2;
+		
+		System.out.println("SOMA = " + soma);
+		
+		dados.close();
+
+	}
+
+}
